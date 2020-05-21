e42f5ae77fb698f99bde86fb7c01d571dc46e44a
orlando <orlandounited@gmail.com>
2020-05-20 21:47:50 -0500

    Revert "test commit"
    This reverts commit acf11221c72e560246ad047fd4372f29b7510a01.

--- JenkinsWebApi/Controllers/ValuesController.cs
+++ JenkinsWebApi/Controllers/ValuesController.cs
@@ -19,7 +19,6 @@ namespace JenkinsWebApi.Controllers
             Information info2 = new Information(2000, "Project Docker", "integrate .net Core with jenkins", DateTime.Now);
             Information info3 = new Information(3000, "Project .Net Core", "Generate a .net core application", DateTime.Now);
             Information info4 = new Information(4000, "Project Integration", "Generate an Integration linux/Microsoft", DateTime.Now);
-            Information info6 = new Information(4000, "Project Integration", "Generate an Integration linux/Microsoft", DateTime.Now);
 
             _list.Add(info1);
             _list.Add(info2);
